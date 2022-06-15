6546356af9d2776ba2cbde14012c5181a2a62ecb
Karol Peła <11260208+irseq@users.noreply.github.com>
2022-06-15 15:27:13 +0200

    Add watchlist relationship and Migration

--- FinanceApp/FinanceApp/Server/Data/ApplicationDbContext.cs
+++ FinanceApp/FinanceApp/Server/Data/ApplicationDbContext.cs
@@ -1,17 +1,51 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
 using FinanceApp.Server.Models;
+using FinanceApp.Server.Models.TickerDetails;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Results = FinanceApp.Server.Models.TickerDetails.Results;
 
 namespace FinanceApp.Server.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<TickerDetails> TickerDetails { get; set; }
+
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TickerDetails>(e =>
+            {
+                e.HasKey(td => td.TickerId);
+                e.ToTable("TickerDetails");
+
+                e.HasMany(td => td.ApplicationUsers)
+                    .WithMany(td => td.TickerWatchlist)
+                    .UsingEntity(j => j.ToTable("TickerDetailsApplicationUser"));
+            });
+
+            builder.Entity<Address>(e =>
+            {
+                e.HasKey(a => a.Address1);
+            });
+
+            builder.Entity<Branding>(e =>
+            {
+                e.HasKey(b => b.LogoUrl);
+            });
+
+            builder.Entity<Results>(e =>
+            {
+                e.HasKey(r => r.Ticker);
+            });
         }
     }
 }
